032017edb692340f5ce9ce195a07f2fdc345e7a5
csc <csc@DESKTOP-UBFUUE0>
2021-07-25 14:33:20 +0530

    Add project files.

---
+++ calc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            Console.WriteLine("Type a number and then press Enter");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type another number, and then press Enter");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Substract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your option?");
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine("Your result: {num1}+{num2} =" + (num1 + num2));
+                    break;
+                case "s":
+                    Console.WriteLine("Your result: {num1}-{num2} =" + (num1 - num2));
+                    break;
+                case "m":
+                    Console.WriteLine("Your result: {num1}*{num2} =" + (num1 * num2));
+                    break;
+                case "d":
+                    Console.WriteLine("Your result: {num1}/{num2} =" + (num1 / num2));
+                    break;
+
+            }
+            Console.Write("Press any key to close the Calculator console app ");
+            Console.ReadKey();
+        }
+    }
+}
